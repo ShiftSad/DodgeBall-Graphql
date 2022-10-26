3700b155ea01d5308cae329ce33a2e8f26f62803
ShiftSad <rosalimavictor@gmail.com>
2022-10-26 10:46:18 -0300

    [😏] Created cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
-import { ApolloServer, gql } from 'apollo-server-express';
+import { ApolloServer } from 'apollo-server-express';
 import resolvers from './resolver';
 import typeDefs from './typeDefs';
 import mongoose from "mongoose";
 import express from 'express';
 import morgan from "morgan";
 import dotenv from 'dotenv';
+import cors from "cors";
 dotenv.config();
 
 const port = process.env.PORT || 4000;
@@ -15,6 +16,9 @@ async function startServer() {
 
     await server.start();
     app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+    app.use(cors({
+        origin: '135.148.153.168'
+    }))
     server.applyMiddleware({app});
 
     await mongoose.connect(process.env.MONGO_URL, {
